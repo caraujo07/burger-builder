26d97ba0c60eb57d440df8459d8d6ba17bc9ca25
caraujo07 <caraujo@jc.com.br>
2019-02-06 16:08:40 -0300

    Mostrando o preço do hamburger no Build Control

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,20 +34,42 @@ class BurgerBuilder extends Component {
     };
     updatedIngredients[type] = updateCount;
     const priceAddition = INGREDIENT_PRICES[type];
-    const newPrice = this.state.totalPrice + priceAddition;
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
   }
 
   removeIngredientHandler = (type) => {
-
+    const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0 ) {
+      return;
+    }
+    const updateCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updateCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
   }
 
   render() {
+    const disableInfo = {
+      ...this.state.ingredients
+    }
+    for(let key in disableInfo) {
+      disableInfo[key] = disableInfo[key] <= 0
+    }
     return (
      <>
       <Burger ingredients={this.state.ingredients} />
       <BuildControls 
-        ingredientAdded={this.addIngredientHandler}/>
+        ingredientAdded={this.addIngredientHandler}
+        ingredientRemoved={this.removeIngredientHandler}
+        disabled={disableInfo}
+        price={this.state.totalPrice}/>
      </>
     );
   }
