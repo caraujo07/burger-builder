69515dae34cac5f892f6998b47357dbbb26df115
caraujo07 <caraujo@jc.com.br>
2019-02-06 14:47:47 -0300

    Adicionando o Handler para adição de novos ingredientes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7  
+};
+
 class BurgerBuilder extends Component {
 
   // constructor(props) {
@@ -15,14 +22,32 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updateCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updateCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const newPrice = this.state.totalPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+  }
+
+  removeIngredientHandler = (type) => {
+
   }
 
   render() {
     return (
      <>
       <Burger ingredients={this.state.ingredients} />
-      <BuildControls />
+      <BuildControls 
+        ingredientAdded={this.addIngredientHandler}/>
      </>
     );
   }
