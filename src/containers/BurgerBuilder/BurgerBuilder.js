63d29978d7780e491f497c40d736120ed40a879e
caraujo07 <caraujo@jc.com.br>
2019-02-06 13:43:41 -0300

    Adicionando os ingredientes de forma dinâmica e verificando se existe algum ingrediente no hamburger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,17 +4,16 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
 
   // constructor(props) {
-  //   super(props) {
+  //   super(props);
   //     this.state = {...}
-  //   }
   // }
   
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
 
